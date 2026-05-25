e3a9acc6eb10c2423753f9db15c57e5f8572840a
sravani punnam <123936694+sravanipunnam22@users.noreply.github.com>
2026-05-25 17:56:18 +0530

    Initial commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApi.Models;
+
+namespace EmployeeApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        static List<Employee> employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "Sravani", Role = "Support", Salary = 30000 },
+            new Employee { Id = 2, Name = "John", Role = "Developer", Salary = 50000 }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(employees);
+        }
+    }
+}
